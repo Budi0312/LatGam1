64c782b92fee20ea81030ab4fd80e29b10cb18d7
Budi0312 <72286755+Budi0312@users.noreply.github.com>
2025-11-12 09:50:11 +0700

    belajar event, invoke addscore +-

--- Assets/Script12Nov2025/DeteksiInput.cs
+++ Assets/Script12Nov2025/DeteksiInput.cs
@@ -1,13 +1,17 @@
+using System;
 using UnityEngine;
 
-public class DeteksiInputt : MonoBehaviour
+public class DeteksiInput : MonoBehaviour
 {
+    public Action inputEvent; //jika ingin menambahkan inputEvent harus ada using System;
+
     public Score objectScore;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            objectScore.AddScore();
+            //objectScore.AddScore(); untuk tambah score
+            inputEvent.Invoke(); //untuk memanggil dan input score
         }
     }
 }
--- Assets/Script12Nov2025/Score.cs
+++ Assets/Script12Nov2025/Score.cs
@@ -1,12 +1,26 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Score : MonoBehaviour
 {
+    public DeteksiInput objectDeteksiInput;
 
     public int score;
+
+    private void OnEnable()
+    {
+        //tanda += yang mau Listen
+        objectDeteksiInput.inputEvent += AddScore;
+    }
 
     public void AddScore()
     {
         score = score + 1;
+    }
+
+    private void OnDisable()
+    {
+        /*tanda -= yang mau UnListen*/
+        objectDeteksiInput.inputEvent -= AddScore;
     }
 }
