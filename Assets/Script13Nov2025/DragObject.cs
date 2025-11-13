076b52350e648a0abda6896dd0d2f9eb55468ddc
Budi0312 <72286755+Budi0312@users.noreply.github.com>
2025-11-13 17:26:16 +0700

    position segala arah

--- Assets/Script13Nov2025/DragObject.cs
+++ Assets/Script13Nov2025/DragObject.cs
@@ -6,10 +6,12 @@ public class DragObject : MonoBehaviour
     void OnMouseDrag()
     {
         Vector2 worldMousePosition=Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 squarePosition = new Vector2(worldMousePosition.x,transform.position.y);
+        Vector2 squarePosition = new Vector2(worldMousePosition.x,worldMousePosition.y); //jika ingin satu arah sesuai garis x maka menggunakan 
         Vector2 minimum = Camera.main.ScreenToWorldPoint(new Vector3(0,0,0));
-        Vector2 maximum = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,0,0));
+        Vector2 maximum = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,Screen.height,0));
         squarePosition.x=Mathf.Clamp(squarePosition.x, minimum.x,maximum.x);
+        squarePosition.y=Mathf.Clamp(squarePosition.y, minimum.y,maximum.y);
+
         //squarePosition.x=Mathf.Clamp(squarePosition.x, minimum.x,maximum.x);
         //squarePosition.x=Mathf.Clamp(squarePosition.x, -8.,8); //Mathf.Clamp untuk membatasi gerak dari objek di layar game kiri dan kanan
         transform.position=squarePosition; //squarePosition untuk mengendalikan di sumbu X, sedangkan jika menggunakan worldMousePosition akan bisa di kendalikan segala posisi
